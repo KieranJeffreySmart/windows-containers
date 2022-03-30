293f9747da9dd9723b359d8705da1e14d8e68042
Kieran Jeffrey-Smart <kjeffreysmar@vmware.com>
2022-03-30 11:28:57 +0100

    Add dockerfile and deployment manifest

---
+++ apps/ASP.NET_Framework/hello-world.aspnet-framework/hello-world-aspnet-framework-4-6-1/Controllers/HelloWorldController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace HelloWorld.AsyncProxy.Framework
+{
+    public class HelloWorldController : ApiController
+    {
+        private readonly ILogger<HelloWorldController> logger;
+
+        public HelloWorldController()
+        {
+            logger = new LoggerFactory().CreateLogger<HelloWorldController>();
+        }
+
+        [HttpGet()]
+        public string HelloWorld()
+        {
+            logger.LogInformation(1000, $"Get()");
+            return "Hello World!!!";
+        }
+
+        [HttpGet()]
+        [Route("Hello")]
+        public string HelloNames([FromUri] string names)
+        {
+            logger.LogInformation(1000, $"GetSync(names:{names})");
+            var hellos = new List<string>();
+            foreach (var name in names.Split(','))
+            {
+                hellos.Add($"Hello {name}!!!");
+            }
+            logger.LogInformation(1001, $"GetSync(names:{names})");
+            return String.Join("\r\n", hellos);
+        }
+
+        [HttpGet()]
+        [Route("HelloEx")]
+        public string ExceptionalHelloWorld()
+        {
+            throw new Exception("Hello World!!!");
+        }
+    }
+}

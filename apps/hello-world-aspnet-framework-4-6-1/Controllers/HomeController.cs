1322f89991da0041194a016ed41300e3770e1054
Kieran Jeffrey-Smart <kjeffreysmar@vmware.com>
2022-04-21 09:51:30 +0100

    Add health check endpoints

--- apps/hello-world-aspnet-framework-4-6-1/Controllers/HomeController.cs
+++ apps/hello-world-aspnet-framework-4-6-1/Controllers/HomeController.cs
@@ -8,11 +8,45 @@ namespace hello_world_aspnet_framework_4_6_1.Controllers
 {
     public class HomeController : Controller
     {
+        int timeToLive = 60;
+        int timeToUnhealthy = 30;
+        DateTime timeStarted = DateTime.Now;
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        public ActionResult Alive()
+        {
+            return new HttpStatusCodeResult((isAlive() ? 200 : 500));
+        }
+
+        public ActionResult Healthy()
+        {
+            return new HttpStatusCodeResult((isHealthy() ? 200 : 500));
+        }
+
+        private bool isAlive()
+        {
+            if (timeStarted.Second+timeToLive < DateTime.Now.Second)
+            {
+                return false;
+            }
+
+            return true;
+        }        
+
+        private bool isHealthy()
+        {
+            if (timeStarted.Second+timeToUnhealthy < DateTime.Now.Second)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
